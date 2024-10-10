b5e8ddf1244c9707239f1f7f53301e7ef740ee23
darrenchia2004 <darrenchia2004@gmail.com>
2024-10-11 07:01:26 +0800

    Console log name.

--- main.js
+++ main.js
@@ -1,2 +1,7 @@
 console.log('Hello World!');
 const name = 'Darren';
+
+// Write a function that console logs your name
+// Add and commit your change with a good brief message
+
+console.log(name);
